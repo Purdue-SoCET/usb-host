ad1999ce1286008f9ed01466ea535774c0986212
William Cunningham <wrcunnin@purdue.edu>
2025-09-03 14:08:25 -0400

    [cicd] adding linter

---
+++ src/usb_host.sv
@@ -0,0 +1,12 @@
+/*
+* Filename:    usb_host.sv
+*
+* Created by:  William Cunningham
+* E-mail:      wrcunnin@purdue.edu
+* Date:        09/03/2025
+* Description: Top level module for the USB Host
+*/
+
+module usb_host ();
+
+endmodule
